394395e7c8c21d53fc235657487bfd70b390db54
Lev Polyakov <www.goldfox@gmail.com>
2019-05-15 19:29:10 +0300

    icon add load image add new custome cell style

---
+++ Places/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Lev polyakov on 15/05/2019.
+//  Copyright © 2019 Lev polyakov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+
+    
+}
--- Places/MainViewController.swift
+++ Places/MainViewController.swift
@@ -18,19 +18,16 @@ class MainViewController: UITableViewController {
     
     // MARK: - Table View Datasourse
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return restarantArray.count }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { return 100.0 }
-    
+        
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell                    = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.textColor  = UIColor.red
-        cell?.backgroundColor       = UIColor.blue
-//        cell.
-        cell?.textLabel?.text       = restarantArray[indexPath.row]
-        cell?.imageView?.layer.cornerRadius = 25
-        cell?.imageView?.clipsToBounds = true
-        cell?.imageView?.image      = UIImage(named: restarantArray[indexPath.row])
-        return cell!
+        let cell                                = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.nameLable.textColor                = UIColor.red
+        cell.backgroundColor                    = UIColor.blue
+        cell.nameLable.text                     = restarantArray[indexPath.row]
+        cell.imageOfPlace.layer.cornerRadius    = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.clipsToBounds         = true
+        cell.imageOfPlace.image                 = UIImage(named: restarantArray[indexPath.row])
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -45,6 +42,9 @@ class MainViewController: UITableViewController {
         }
     }
     
+    // MARK: - Table view deligate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { return 100.0 }
+
     /*
      // MARK: - Navigation
      
